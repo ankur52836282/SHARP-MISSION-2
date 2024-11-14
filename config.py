87c98425c0459926d6f20cce28d88c85fd7e2c67
ankur52836282 <sharpfilesx@outlook.com>
2024-11-14 12:25:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7999202189:AAGSIDqRKwo3spFJkTiynVRlGNpZdOmRTTc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Ryuzaki_xd")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5666606072")
+
+
+
+
+
+
+
+  
